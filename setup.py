838d89083d9b6a77ceb3ee3ebc02d237130b1e70
Nolan Conaway <nolanbconaway@gmail.com>
2022-06-11 18:23:41 -0400

    Make dbt optional

--- setup.py
+++ setup.py
@@ -7,11 +7,6 @@ setup(
     version="0.1.3",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=[
-        "requests==2.27.1",
-        "psycopg2-binary==2.9.3",
-        "tqdm==4.63.0",
-        "dbt-postgres==1.1.0",
-    ],
-    extras_require=dict(test=["black==22.1.0"]),
+    install_requires=["requests==2.27.1", "psycopg2-binary==2.9.3", "tqdm==4.63.0"],
+    extras_require=dict(dbt=["dbt-postgres==1.1.0"], test=["black==22.1.0"]),
 )
