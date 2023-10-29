5d77ec42eaf43c57da6ae5ec000de3765c833f27
Nolan Conaway <nolanbconaway@gmail.com>
2023-10-29 11:51:18 -0400

    drop old files

--- src/moomoo/ingest/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Collect raw data from internet and local sources.
-
-These data should be the first stage in the pipeline. They should not rely on any
-existing data in the database.
-"""
-from . import collect_listen_data, collect_local_files
-
-__all__ = ["collect_listen_data", "collect_local_files"]
