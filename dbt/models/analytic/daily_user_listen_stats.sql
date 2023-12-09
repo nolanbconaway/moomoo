3390b9c2064f3f41985e3dd6a87e33fcec172b2d
Nolan Conaway <nolanbconaway@gmail.com>
2023-12-09 17:54:04 -0500

    expose release group mbid from lookup

--- dbt/models/analytic/daily_user_listen_stats.sql
+++ dbt/models/analytic/daily_user_listen_stats.sql
@@ -2,33 +2,45 @@
 
 {# anaytics on user listens. #}
 
-with t as (
+--- get % of listens that are mapped to a file
+with listen_mapped_prop as (
   select
     listens.username
     , listens.listen_at_ts_utc::date as listen_date
-    , count(distinct listens.listen_md5) as count_listens
     , (
       count(distinct case when map_.filepath is not null then listens.listen_md5 end)::real
       / count(distinct listens.listen_md5)::real
     ) as pct_listens_mapped_to_file
-    , count(distinct listens.recording_mbid) as count_recordings
-    , count(distinct listens.release_mbid) as count_releases
-    , sum(distinct listens.duration_ms) / 3600000::real as sum_listen_hours
 
   from {{ ref('listens') }} as listens
-  -- NOTE: one to many here. 1 recording can have 0-2 files.
-  -- eventually split this into a cte and join back in.
   left join {{ ref('file_recording_map') }} as map_ using (recording_mbid)
 
   where listens.recording_mbid is not null
-    and listens.listen_at_ts_utc::date >= current_date - interval '90 days'
 
-  group by 1, 2
+  group by listens.username, listens.listen_at_ts_utc::date
+)
+
+, counts as (
+  select
+    username
+    , listen_at_ts_utc::date as listen_date
+    , count(listen_md5) as count_listens
+    , count(recording_mbid) as count_recordings
+    , count(release_mbid) as count_releases
+    , sum(duration_ms) / 3600000::real as sum_listen_hours
+
+  from {{ ref('listens') }}
+  where listen_at_ts_utc::date >= current_date - interval '90 days'
+
+  group by username, listen_at_ts_utc::date
 )
 
 select
   {{ dbt_utils.generate_surrogate_key(['username', 'listen_date']) }} as user_date_key
-  , *
+  , counts.*
+  , listen_mapped_prop.pct_listens_mapped_to_file
 
-from t
-order by 1, 2 desc
+from counts
+left join listen_mapped_prop using (username, listen_date)
+
+order by counts.username asc, counts.listen_date desc
--- dbt/models/listenbrainz/messybrainz_name_map.sql
+++ dbt/models/listenbrainz/messybrainz_name_map.sql
@@ -10,14 +10,30 @@
 ) }}
 
 {# Payloads like:
-  {
-    "artist_mbids": ["084308bd-1654-436f-ba03-df6697104e19"],
-    "release_mbid": "fd3c6333-9e3e-4360-aff7-05c0512e8b38",
-    "release_name": "Kerplunk!",
-    "recording_mbid": "cef5e2a8-0272-4dff-84c6-88721af19b2f",
-    "recording_name": "Welcome to Paradise",
-    "artist_credit_name": "Green Day"
-  }
+{
+  "metadata": {
+    "release": {
+      "mbid": "8549ce34-2349-42e0-bf28-1a773b35bcaa",
+      "name": "The Soft Bulletin",
+      "year": 1999,
+      "caa_id": 23592292976,
+      "caa_release_mbid": "8549ce34-2349-42e0-bf28-1a773b35bcaa",
+      "album_artist_name": "The Flaming Lips",
+      "release_group_mbid": "1a021034-95d1-3a2d-bdca-73b25e455e49"
+    },
+    "recording": {
+      "name": "A Spoonful Weighs a Ton",
+      "rels": [],
+      "length": 212000
+    }
+  },
+  "artist_mbids": ["1f43d76f-8edf-44f6-aaf1-b65f05ad9402"],
+  "release_mbid": "8549ce34-2349-42e0-bf28-1a773b35bcaa",
+  "release_name": "The Soft Bulletin",
+  "recording_mbid": "6deeee64-d457-4c72-be51-9d387dafecec",
+  "recording_name": "A Spoonful Weighs a Ton",
+  "artist_credit_name": "The Flaming Lips"
+}
 #}
 
 
@@ -28,6 +44,9 @@ select
   , {{ json_get('payload_json', ['artist_mbids'], as_json=True) }} as artist_mbids
   , {{ try_cast_uuid(json_get('payload_json', ['recording_mbid'])) }} as recording_mbid
   , {{ try_cast_uuid(json_get('payload_json', ['release_mbid'])) }} as release_mbid
+  , (
+    {{ try_cast_uuid(json_get('payload_json', ['metadata', 'release', 'release_group_mbid'])) }}
+  ) as release_group_mbid
   , {{ json_get('payload_json', ['release_name']) }}::varchar as mapped_release_name
   , {{ json_get('payload_json', ['recording_name']) }}::varchar as mapped_recording_name
   , {{ json_get('payload_json', ['artist_credit_name']) }}::varchar as mapped_artist_name
