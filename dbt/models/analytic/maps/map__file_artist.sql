57b86ec0d46f94c695c10fbc3efb1b315ec7a51a
Nolan Conaway <nolanbconaway@gmail.com>
2024-01-15 10:43:59 -0500

    expand artist file map to include album artist

--- dbt/models/analytic/maps/map__file_artist.sql
+++ dbt/models/analytic/maps/map__file_artist.sql
@@ -4,7 +4,7 @@
   )
 }}
 
-{# Map files to potential recording mbids. #}
+{# Map files to potential artist mbids. #}
 
 select local_files.filepath, artist_mbid_values.value::uuid as artist_mbid
 
@@ -15,6 +15,7 @@ inner join {{ ref('messybrainz_name_map') }} as map_ using (recording_md5)
 where map_.artist_mbids is not null
   and jsonb_array_length(map_.artist_mbids) > 0
   -- exclude a custom library that does not map to anything on musicbrainz
+  -- this ends up being noisy
   and local_files.filepath not like 'chopnscrew/%'
 
 union distinct
@@ -22,3 +23,9 @@ union distinct
 select filepath, artist_mbid
 from {{ ref('local_files') }}
 where artist_mbid is not null
+
+union distinct
+
+select filepath, album_artist_mbid as artist_mbid
+from {{ ref('local_files') }}
+where album_artist_mbid is not null
