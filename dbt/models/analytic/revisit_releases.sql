f74be1502b06f3a94d38a965e14fc6f0f5813974
Nolan Conaway <nolanbconaway@gmail.com>
2024-03-03 13:53:22 -0500

    use revisit score in revisit releases

--- dbt/models/analytic/revisit_releases.sql
+++ dbt/models/analytic/revisit_releases.sql
@@ -16,35 +16,24 @@
 
 #}
 
-{% set ts="current_timestamp - interval '90 days'" %}
-
-with release_group_stats as (
-  select
-    releases.release_group_mbid
-    , listens.username
-    , count(distinct listens.recording_mbid) as num_recordings
-    , sum(case when listens.listen_at_ts_utc < {{ ts }} then 1 end) as listens_old
-    , sum(case when listens.listen_at_ts_utc >= {{ ts }} then 1 end) as listens_recent
-
-  from {{ ref('listens') }} as listens
-  inner join {{ ref('releases') }} as releases using (release_mbid)
-
-  group by 1, 2
-  -- idk about singles or huge box sets
-  having count(distinct listens.recording_mbid) between 4 and 20
-)
-
 select
-  stats_.release_group_mbid
-  , release_groups.release_group_title as release_group_title
-  , release_groups.artist_credit_phrase as artist_name
-  , stats_.username
-  , stats_.num_recordings
-  , stats_.listens_old
-  , stats_.listens_recent
+  release_group_mbid
+  , release_group_title
+  , artist_credit_phrase as artist_name
+  , username
+  , revisit_score
+  , lifetime_recording_count as num_recordings
+  , lifetime_listen_count - last90_listen_count as listens_old
+  , last90_listen_count as listens_recent
 
-from release_group_stats as stats_
-inner join {{ ref('release_groups') }} as release_groups using (release_group_mbid)
-where stats_.listens_old > (stats_.num_recordings * 2)
-  and stats_.listens_old > 10
-  and stats_.listens_recent < 10
+from {{ ref('release_group_listen_counts') }}
+
+-- idk about singles or huge box sets
+where lifetime_recording_count between 4 and 20
+  and last90_listen_count < 10
+  and (lifetime_listen_count - last90_listen_count) > 10
+  and (lifetime_listen_count - last90_listen_count) > (lifetime_recording_count * 2)
+
+  -- baseline revisit score of 30 days recency * 5 tracks = (1 - 0.05) * ln(5)
+  and revisit_score > 1.5
+order by revisit_score desc
