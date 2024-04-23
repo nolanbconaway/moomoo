12cff32b60cd763e1fdd2134a842aabdaf72eff2
Nolan Conaway <nolanbconaway@gmail.com>
2024-04-23 17:16:53 -0400

    fix release additions (#153)
    * fix release additions
    
    * version
    
    * change score name
    
    * lint

--- dbt/models/analytic/library_release_additions.sql
+++ dbt/models/analytic/library_release_additions.sql
@@ -7,54 +7,54 @@
 
 with similar_users as (
   select
-    from_username as username
-    , time_range
-    , mbid as release_mbid
-    , sum(user_similarity * log(listen_count)) as score
+    sua.from_username as username
+    , sua.time_range
+    , releases.release_group_mbid
+    , sum(sua.user_similarity * log(sua.listen_count)) as score
 
-  from {{ ref('similar_user_activity') }}
+  from {{ ref('similar_user_activity') }} as sua
+  inner join {{ ref('releases') }} as releases
+    on sua.mbid = releases.release_mbid
 
-  where entity = 'release'
+  where sua.entity = 'release'
 
   group by 1, 2, 3
 )
 
 
 , known_releases as (
-  select distinct releases.release_mbid
+  select distinct release_group_mbid
   from {{ ref('map__file_release_group') }}
-  inner join {{ ref('releases') }} as releases using (release_group_mbid)
 )
 
 , scores as (
   select
     similar_users.username
     , similar_users.time_range
-    , similar_users.release_mbid
-    , similar_users.score as similarity
+    , similar_users.release_group_mbid
+    , similar_users.score
     , row_number() over (
       partition by similar_users.username, similar_users.time_range
       order by similar_users.score desc
     ) as rank
 
   from similar_users
-  left join known_releases using (release_mbid)
+  left join known_releases using (release_group_mbid)
 
-  where known_releases.release_mbid is null
+  where known_releases.release_group_mbid is null
 )
 
 select
   scores.username
   , scores.time_range
   , scores.rank
-  , scores.release_mbid
-  , releases.artist_credit_phrase
-  , releases.release_title
-
-  , scores.similarity
+  , scores.release_group_mbid
+  , release_groups.artist_credit_phrase
+  , release_groups.release_group_title
+  , scores.score
 
 from scores
-inner join {{ ref('releases') }} as releases using (release_mbid)
+inner join {{ ref('release_groups') }} as release_groups using (release_group_mbid)
 
 where scores.rank <= 200
 order by scores.username, scores.time_range, scores.rank
