b608e1a66045cf695dc4c2d71b15898e954d1dbe
Nolan Conaway <nolanbconaway@gmail.com>
2024-07-28 17:46:05 -0400

    improve revisit logic

--- dbt/models/analytic/revisit_tracks.sql
+++ dbt/models/analytic/revisit_tracks.sql
@@ -18,17 +18,17 @@ with recordings as (
     , artist_credit_phrase as artist_name
     , username
     , revisit_score
-    , lifetime_listen_count - last90_listen_count as listens_old
+    , last150_listen_count - last90_listen_count as listens_old
     , last90_listen_count as listens_recent
 
   from {{ ref('recording_listen_counts') }}
 
-  -- idk about singles or huge box sets
-  where last90_listen_count < 3
-    and (lifetime_listen_count - last90_listen_count) > 3
+  where last90_listen_count <= 2 -- only a few listens in the last 90 days
+    and last14_listen_count = 0 -- no listens in the last 14 days
+    and (last150_listen_count - last90_listen_count) >= 2 -- at least 2 listens older than 90 days
 
-    -- baseline revisit score of 30 days recency * 5 listens = (1 - 0.05) * ln(5)
-    and revisit_score > 1.5
+    -- baseline revisit score of 30 days recency * 3 listens = (1 - 0.05) * ln(3)
+    and revisit_score > 1.05
 )
 
 -- attach a file, artist mbids. use distinct on to select a single file for each recording
