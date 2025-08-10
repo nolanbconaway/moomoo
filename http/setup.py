0d8a134f79a0011e37b923f0ab35c5110ee4b63f
Nolan Conaway <nolanbconaway@gmail.com>
2025-08-10 11:26:58 -0400

    postgres 16

--- http/setup.py
+++ http/setup.py
@@ -12,7 +12,7 @@ version = (
 
 # setup for installing moomoo-playlist from a git repository
 # TODO: get the url from shell? grab from a tagged release?
-playlist_commit = "d9840165e1b474cfc3e8576e76fe661e136a341c"
+playlist_commit = "59af9b23daa9813038bc04d43281c09d9c536b62"
 playlist_url = "https://github.com/nolanbconaway/moomoo.git"
 
 setup(
