de93c52a68767433f9a7cc6c4026e0b63455aa2e
Nolan Conaway <nolanbconaway@gmail.com>
2025-10-05 22:26:48 -0400

    extend release group model

--- dbt/models/musicbrainz/release_groups.sql
+++ dbt/models/musicbrainz/release_groups.sql
@@ -10,9 +10,14 @@
 select
   mbid as release_group_mbid
   , {{ json_get('payload_json', ["data", "release-group", "title"]) }} as release_group_title
+  , {{ json_get('payload_json', ["data", "release-group", "release-count"]) }} as release_count
   , {{ extract_year(
       json_get('payload_json', ["data", "release-group", "first-release-date"])
     ) }} as release_group_year
+  , {{ json_get('payload_json', ["data", "release-group", "first-release-date"]) }} as first_release_date
+  , {{ json_get('payload_json', ["data", "release-group", "type"]) }} as release_group_type
+  , {{ json_get('payload_json', ["data", "release-group", "primary-type"]) }} as release_group_primary_type
+  , {{ json_get('payload_json', ["data", "release-group", "release-count"]) }} as release_count
   , {{ json_get('payload_json', ["data", "release-group", "artist-credit"], as_json=True) }} as artist_credit_list
   , {{ json_get('payload_json', ["data", "release-group", "tag-list"], as_json=True) }} as tag_list
   , {{ json_get('payload_json', ["data", "release-group", "url-relation-list"], as_json=True) }} as url_relation_list
