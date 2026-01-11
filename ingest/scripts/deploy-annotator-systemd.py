3aff18e2b7676c89159664dca496753c0bcff2ed
Nolan Conaway <nolanbconaway@gmail.com>
2026-01-11 11:45:38 -0500

    ruff

--- ingest/scripts/deploy-annotator-systemd.py
+++ ingest/scripts/deploy-annotator-systemd.py
@@ -16,7 +16,7 @@ SYSTEMD_TARGET = Path(f"/etc/systemd/system/{SYSTEMD_NAME}")
 
 def run_command(command, *args, **kwargs):
     """Use subprocess to run a command, printing it first.'
-    
+
     Needed here because we need sudo for some commands.
     """
     print("Running: " + " ".join(map(str, command)))
